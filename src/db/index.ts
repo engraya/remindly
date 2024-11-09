db84cd1063207038c085d9682c7670d6f74e7dc1
Engraya <ahmadyakubuaya@gmail.com>
2024-11-09 23:40:17 +0100

    Fixed Build Error

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,6 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 
 const sql = neon(process.env.DATABASE_URL!);
-//@ts-ignore
+
+// @ts-expect-error: This line might cause a type issue depending on the structure of drizzle's client.
 export const db = drizzle({ client: sql });
