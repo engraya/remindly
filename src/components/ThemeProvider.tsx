26fc650c7c5f9ac24531d6a0b37d26f94a0d63ac
Engraya <ahmadyakubuaya@gmail.com>
2024-11-09 23:52:15 +0100

    Fixed error

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+// @ts-expect-error: This line might cause a type issue depending on the structure of drizzle's client.
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
